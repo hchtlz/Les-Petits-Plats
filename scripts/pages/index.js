fb05fb2eeceebca6642460d7cd4059021280ffe3
Hugo Chtlz <93914147+hchtlz@users.noreply.github.com>
2023-03-29 10:47:20 +0200

    FIX : Inputs repeat contents and add tags

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -2,7 +2,7 @@ import { getRecipes } from '../utils/model.js'
 
 const recipes = getRecipes()
 
-// Construction of the Card
+// Construction of the card
 const mainIndexResults = document.querySelector('.main-index__results-container')
 recipes.forEach(recipe => {
 	const card = document.createElement('div')
@@ -32,56 +32,103 @@ recipes.forEach(recipe => {
 	mainIndexResults.appendChild(card)
 })
 
-// récuperer tous les ingredients présents dans le json
+// get data from the json file
 const allIngredients = recipes.map(recipe => recipe.ingredients.map(ingredient => ingredient.ingredient)).flat()
 
 // dropdown menu
+const dropdownMenu = document.querySelectorAll('.dropdown-menu')
 const dropdownMenuIngredients = document.querySelector('.dropdown-menu--ingredients')
 const dropdownMenuAppliances = document.querySelector('.dropdown-menu--devices')
 const dropdownMenuUstensils = document.querySelector('.dropdown-menu--utensils')
 
+// dropdown menu ingredients
 dropdownMenuIngredients.addEventListener('click', () => {
   dropdownMenuIngredients.classList.toggle('is-active')
   dropdownMenuIngredients.querySelector('.dropdown-menu__options').classList.toggle('is-active')
+  const dropdownMenuOptions = dropdownMenuIngredients.querySelector('.dropdown-menu__options')
+  
+  if (dropdownMenuOptions.classList.contains('is-active')) {
+    
+    const input = dropdownMenuIngredients.querySelector('.dropdown-menu__sort-input')
+    input.value = ''
+    
+    const uniqueIngredients = [...new Set(allIngredients)]
+    dropdownMenuOptions.innerHTML = uniqueIngredients.map(ingredient => `<li class="dropdown-menu__option ingredients">${ingredient}</li>`).join('')
+  
+  } else {
+    dropdownMenuOptions.innerHTML = ''
+
+    const input = dropdownMenuIngredients.querySelector('.dropdown-menu__sort-input')
+    input.value = 'Ingrédients'
+  }
 })
 
+// dropdown menu appliances
 dropdownMenuAppliances.addEventListener('click', () => {
   dropdownMenuAppliances.classList.toggle('is-active')
   dropdownMenuAppliances.querySelector('.dropdown-menu__options').classList.toggle('is-active')
+  const dropdownMenuOptions = dropdownMenuAppliances.querySelector('.dropdown-menu__options')
+  
+  if (dropdownMenuOptions.classList.contains('is-active')) {
+    
+    const input = dropdownMenuAppliances.querySelector('.dropdown-menu__sort-input')
+    input.value = ''
+
+    const allAppliances = recipes.map(recipe => recipe.appliance)
+    const uniqueAppliances = [...new Set(allAppliances)]
+    dropdownMenuOptions.innerHTML = uniqueAppliances.map(appliance => `<li class="dropdown-menu__option devices">${appliance}</li>`).join('')
+  
+  } else {
+    dropdownMenuOptions.innerHTML = ''
+
+    const input = dropdownMenuAppliances.querySelector('.dropdown-menu__sort-input')
+    input.value = 'Appareils'
+  }
 })
 
+// dropdown menu ustensils
 dropdownMenuUstensils.addEventListener('click', () => {
   dropdownMenuUstensils.classList.toggle('is-active')
   dropdownMenuUstensils.querySelector('.dropdown-menu__options').classList.toggle('is-active')
-})
-
-
-// liste des ingrédients
-dropdownMenuIngredients.addEventListener('click', () => {
-  const dropdownMenuOptions = dropdownMenuIngredients.querySelector('.dropdown-menu__options')
-  if (dropdownMenuOptions.classList.contains('is-active')) {
-    dropdownMenuOptions.innerHTML = allIngredients.map(ingredient => `<li class="dropdown-menu__option ingredients">${ingredient}</li>`).join('')
-  } else {
-    dropdownMenuOptions.innerHTML = ''
-  }
-})
-
-// liste des appareils
-dropdownMenuAppliances.addEventListener('click', () => {
-  const dropdownMenuOptions = dropdownMenuAppliances.querySelector('.dropdown-menu__options')
-  if (dropdownMenuOptions.classList.contains('is-active')) {
-    dropdownMenuOptions.innerHTML = recipes.map(recipe => `<li class="dropdown-menu__option appliances">${recipe.appliance}</li>`).join('')
-  } else {
-    dropdownMenuOptions.innerHTML = ''
-  }
-})
-
-// liste des ustensiles
-dropdownMenuUstensils.addEventListener('click', () => {
   const dropdownMenuOptions = dropdownMenuUstensils.querySelector('.dropdown-menu__options')
+  
   if (dropdownMenuOptions.classList.contains('is-active')) {
-    dropdownMenuOptions.innerHTML = recipes.map(recipe => recipe.ustensils.map(ustensil => `<li class="dropdown-menu__option utensils">${ustensil}</li>`)).flat().join('')
+    
+    const input = dropdownMenuUstensils.querySelector('.dropdown-menu__sort-input')
+    input.value = ''
+    
+    const allUstensils = recipes.map(recipe => recipe.ustensils).flat()
+    const uniqueUstensils = [...new Set(allUstensils)]
+    dropdownMenuOptions.innerHTML = uniqueUstensils.map(ustensil => `<li class="dropdown-menu__option utensils">${ustensil}</li>`).join('')
+  
   } else {
     dropdownMenuOptions.innerHTML = ''
+    
+    const input = dropdownMenuUstensils.querySelector('.dropdown-menu__sort-input')
+    input.value = 'Ustensiles'
   }
+})
+
+// when clicking on an ingredient, it is added to the search bar
+dropdownMenu.forEach(menu => {
+  menu.addEventListener('click', (e) => {
+    if (e.target.classList.contains('dropdown-menu__option')) {
+      const input = menu.querySelector('.dropdown-menu__sort-input')
+      input.value = e.target.textContent
+
+      // to each ingredient added to the search bar, a tag is created with name and cross
+      const searchResults = document.querySelector('.main-index__tags-container')
+      const tag = document.createElement('div')
+      tag.classList.add('tag')
+      tag.innerHTML = `
+        <p class="tag__text">${e.target.textContent}</p>
+        <i class="fas fa-times tag__cross"></i>
+      `
+      // when clicking on the cross, the tag is removed
+      tag.querySelector('.tag__cross').addEventListener('click', () => {
+        tag.remove()
+      })
+      searchResults.appendChild(tag)
+    }
+  })
 })
