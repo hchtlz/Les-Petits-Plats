77a91b8017ece970f516d8057b737ca8874fa1eb
Hugo Chtlz <93914147+hchtlz@users.noreply.github.com>
2023-04-02 00:08:41 +0200

    TRY : fix sort function error

--- scripts/utils/sorting.js
+++ scripts/utils/sorting.js
@@ -15,7 +15,6 @@ export function sorting(){
       const filteredRecipes = recipes.filter((recipe) => {
         return recipe.name.toLowerCase().includes(searchBarValue.toLowerCase())
       })
-      console.log(filteredRecipes)
 
       // afficher les recettes qui correspondent a la recherche dans le DOM 
       const recipesContainer = document.querySelector('.main-index__results-container')
@@ -48,19 +47,48 @@ export function sorting(){
         `
         recipesContainer.appendChild(card)
       })
-    }
-    // afficher message d'erreur si la recherche ne correspond a aucune recette
-    else if (searchBarValue.length < 3) {
+    } else if (searchBarValue.length < 3) {
       const recipesContainer = document.querySelector('.main-index__results-container')
       recipesContainer.innerHTML = ''
-      // afficher image d'erreur et un message d'erreur dans une div
+      recipes.forEach((recipe) => {
+        const card = document.createElement('div')
+        card.classList.add('card')
+        card.innerHTML = `
+          <div class="card__image"></div>
+          <div class="card__text-container">
+            <div class="card__header">
+              <h2 class="card__header-title">${recipe.name}</h2>
+              <div class="card__header-time">
+                <i class="far fa-clock"></i>
+                <p class="card__header-time--text">${recipe.time} min</p>
+              </div>
+            </div>
+            <div class="card__recipe-details">
+            <p class="card__recipe-details--description">${recipe.description}</p>
+            <ul class="card__recipe-details--ingredients-list">
+              ${recipe.ingredients.map(object => `
+              <li class="card__recipe-details--ingredient">
+              <span class="card__recipe-details--ingredient-name">${object.ingredient? object.ingredient : ''}:</span>
+              <span class="card__recipe-details--ingredient-quantity">${object.quantity? object.quantity : ''}</span>
+              <span class="card__recipe-details--ingredient-unit">${object.unit? object.unit : ''}</span>
+              </li>`).join('')}
+            </ul>
+          </div>
+        `
+        recipesContainer.appendChild(card)
+      })
+      //////////     DON'T WORK TO DISPLAY ERROR MESSAGE WHEN NO RESULTS FOUND     //////////
+    } else if (searchBarValue.length < 3 && filteredRecipes.length === 0 ) {
+      const recipesContainer = document.querySelector('.main-index__results-container')
+      recipesContainer.innerHTML = ''
       const errorDiv = document.createElement('div')
       errorDiv.classList.add('error')
       errorDiv.innerHTML = `
         <img class="main-index__error-image" src="assets/images/no-results.png" alt="error">
-        <p class="main-index__error-text">Aucune recette ne correspond à votre critère...</p>
+        <p class="main-index__error-text">Aucune recette ne correspond à votre recherche...</p>
       `
       recipesContainer.appendChild(errorDiv)
     }
+    console.log(filteredRecipes.length)
   })
 }
