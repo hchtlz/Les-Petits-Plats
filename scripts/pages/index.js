c1b3d9665b1cbb842f8e3f9da0ca0ed697130b8e
Hugo Chtlz <93914147+hchtlz@users.noreply.github.com>
2023-05-17 14:33:08 +0200

    UPD : refacto code

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,9 +1,9 @@
 import { getRecipes } from '../utils/model.js'
-import { sorting } from '../utils/sorting.js'
+import { render } from '../utils/render.js'
 import { createCard } from '../utils/card.js'
-import { filterIngredients } from '../utils/sorting.js'
-import { filterAppliances } from '../utils/sorting.js'
-import { filterUstensils } from '../utils/sorting.js'
+import { filterIngredients } from '../utils/render.js'
+import { filterAppliances } from '../utils/render.js'
+import { filterUstensils } from '../utils/render.js'
 
 const recipes = getRecipes() // Get the recipes from the model
 const dropdownMenu = document.querySelectorAll('.dropdown-menu')
@@ -14,35 +14,35 @@ const mainIndexResults = document.querySelector('.main-index__results-container'
 
 // Construction of the card
 for (let i = 0; i < recipes.length; i++) {
-  const card = createCard(recipes[i])
-  mainIndexResults.appendChild(card)
+	const card = createCard(recipes[i])
+	mainIndexResults.appendChild(card)
 } 
 
 // Sorting of the ingredients, appliances and ustensils
 filterIngredients()
-filterAppliances();
-filterUstensils();
+filterAppliances()
+filterUstensils()
 
 /**
  * Toggle dropdown menu and display, delete the value of the input
  **/
 function toggleDropdown(dropdownMenu, label) {
-  dropdownMenu.addEventListener('click', () => {
-    // Toggle the active class on the dropdown menu and its options
-    dropdownMenu.classList.toggle('is-active');
-    dropdownMenu.querySelector('.dropdown-menu__options').classList.toggle('is-active');
-    const dropdownMenuOptions = dropdownMenu.querySelector('.dropdown-menu__options');
+	dropdownMenu.addEventListener('click', () => {
+		// Toggle the active class on the dropdown menu and its options
+		dropdownMenu.classList.toggle('is-active')
+		dropdownMenu.querySelector('.dropdown-menu__options').classList.toggle('is-active')
+		const dropdownMenuOptions = dropdownMenu.querySelector('.dropdown-menu__options')
 
-    if (dropdownMenuOptions.classList.contains('is-active')) {
-      // Clear the input value when the dropdown menu is active
-      const input = dropdownMenu.querySelector('.dropdown-menu__sort-input');
-      input.value = '';
-    } else {
-      // Set the input value to the specified label when the dropdown menu is not active
-      const input = dropdownMenu.querySelector('.dropdown-menu__sort-input');
-      input.value = label;
-    }
-  });
+		if (dropdownMenuOptions.classList.contains('is-active')) {
+			// Clear the input value when the dropdown menu is active
+			const input = dropdownMenu.querySelector('.dropdown-menu__sort-input')
+			input.value = ''
+		} else {
+			// Set the input value to the specified label when the dropdown menu is not active
+			const input = dropdownMenu.querySelector('.dropdown-menu__sort-input')
+			input.value = label
+		}
+	})
 }
 
 toggleDropdown(dropdownMenuIngredients, 'Ingrédients')
@@ -53,71 +53,71 @@ toggleDropdown(dropdownMenuUstensils, 'Ustensiles')
  * Search tag in dropdown menu
 **/
 function searchTag(e) {
-  // Get the input element
-  const input = e.target;
+	// Get the input element
+	const input = e.target
   
-  // Convert the search filter to uppercase for case-insensitive search
-  const filter = input.value.toUpperCase();
+	// Convert the search filter to uppercase for case-insensitive search
+	const filter = input.value.toUpperCase()
   
-  // Get all the dropdown menu options
-  const options = input.parentNode.querySelectorAll('.dropdown-menu__option');
+	// Get all the dropdown menu options
+	const options = input.parentNode.querySelectorAll('.dropdown-menu__option')
   
-  // Iterate over each option and filter based on the search filter
-  for (let i = 0; i < options.length; i++) {
-    const text = options[i].textContent || options[i].innerText;
+	// Iterate over each option and filter based on the search filter
+	for (let i = 0; i < options.length; i++) {
+		const text = options[i].textContent || options[i].innerText
     
-    // Check if the option's text contains the search filter
-    if (text.toUpperCase().indexOf(filter) > -1) {
-      options[i].style.display = ''; // Show the option
-    } else {
-      options[i].style.display = 'none'; // Hide the option
-    }
-  }
+		// Check if the option's text contains the search filter
+		if (text.toUpperCase().indexOf(filter) > -1) {
+			options[i].style.display = '' // Show the option
+		} else {
+			options[i].style.display = 'none' // Hide the option
+		}
+	}
 }
 
 // search tag
 for (let i = 0; i < dropdownMenu.length; i++) {
-  dropdownMenu[i].addEventListener('keyup', (e) => {
-    if (e.target.classList.contains('dropdown-menu__sort-input')) {
-      searchTag(e)
-    }
-  })
+	dropdownMenu[i].addEventListener('keyup', (e) => {
+		if (e.target.classList.contains('dropdown-menu__sort-input')) {
+			searchTag(e)
+		}
+	})
 }
 
 // Display tags
 for (let i = 0; i < dropdownMenu.length; i++) {
-  dropdownMenu[i].addEventListener('click', (e) => {
-    if (e.target.classList.contains('dropdown-menu__option')) {
-      // Get the container for search results
-      const searchResults = document.querySelector('.main-index__tags-container');
+	dropdownMenu[i].addEventListener('click', (e) => {
+		if (e.target.classList.contains('dropdown-menu__option')) {
+			// Get the container for search results
+			const searchResults = document.querySelector('.main-index__tags-container')
 
-      // Create a new tag element
-      const tag = document.createElement('div');
-      tag.classList.add('tag');
-      tag.innerHTML = `
+			// Create a new tag element
+			const tag = document.createElement('div')
+			tag.classList.add('tag')
+			tag.innerHTML = `
         <p class="tag__text">${e.target.outerText}</p>
         <img class="tag__cross" src="assets/SVGS/cross.svg" alt="cross">
-      `;
+      `
 
-      // Add event listener to the cross icon of the tag to remove the tag when clicked
-      tag.querySelector('.tag__cross').addEventListener('click', () => {
-        tag.remove();
-      });
+			// Add event listener to the cross icon of the tag to remove the tag when clicked
+			tag.querySelector('.tag__cross').addEventListener('click', () => {
+				tag.remove()
+			})
 
-      // Add a class to the tag based on the selected option
-      if (e.target.classList.contains('ingredients')) {
-        tag.classList.add('ingredients');
-      } else if (e.target.classList.contains('devices')) {
-        tag.classList.add('devices');
-      } else if (e.target.classList.contains('utensils')) {
-        tag.classList.add('utensils');
-      }
+			// Add a class to the tag based on the selected option
+			if (e.target.classList.contains('ingredients')) {
+				tag.classList.add('ingredients')
+			} else if (e.target.classList.contains('devices')) {
+				tag.classList.add('devices')
+			} else if (e.target.classList.contains('utensils')) {
+				tag.classList.add('utensils')
+			}
 
-      // Append the tag to the search results container
-      searchResults.appendChild(tag);
-    }
-  });
+			// Append the tag to the search results container
+			searchResults.appendChild(tag)
+		}
+	})
 }
 
 // display recipes
-sorting();
+render()
--- scripts/utils/card.js
+++ scripts/utils/card.js
@@ -1,8 +1,8 @@
 export function createCard(recipe) {
-  // Create a new card element
-  const card = document.createElement('div');
-  card.classList.add('card');
-  card.innerHTML = `
+	// Create a new card element
+	const card = document.createElement('div')
+	card.classList.add('card')
+	card.innerHTML = `
     <div class="card__image"></div>
     <div class="card__text-container">
       <div class="card__header">
@@ -18,21 +18,21 @@ export function createCard(recipe) {
         </ul>
       </div>
     </div>
-  `;
+  `
 
-  // Insert the ingredients into the card
-  const ingredientsList = card.querySelector('.card__recipe-details--ingredients-list');
+	// Insert the ingredients into the card
+	const ingredientsList = card.querySelector('.card__recipe-details--ingredients-list')
   
-  for (const ingredient of recipe.ingredients) {
-    const ingredientElement = document.createElement('li');
-    ingredientElement.classList.add('card__recipe-details--ingredient');
-    ingredientElement.innerHTML = `
+	for (const ingredient of recipe.ingredients) {
+		const ingredientElement = document.createElement('li')
+		ingredientElement.classList.add('card__recipe-details--ingredient')
+		ingredientElement.innerHTML = `
       <span class="card__recipe-details--ingredient-name">${ingredient.ingredient ? ingredient.ingredient : ''}:</span>
       <span class="card__recipe-details--ingredient-quantity">${ingredient.quantity ? ingredient.quantity : ''}</span>
       <span class="card__recipe-details--ingredient-unit">${ingredient.unit ? ingredient.unit : ''}</span>
-    `;
-    ingredientsList.appendChild(ingredientElement);
-  }
+    `
+		ingredientsList.appendChild(ingredientElement)
+	}
   
-  return card;
+	return card
 }
